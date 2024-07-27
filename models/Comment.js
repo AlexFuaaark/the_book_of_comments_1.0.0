63d73497a79fa72780fa52948b53e525c27879dc
AlexFuaaark <alexfuaaark@icloud.com>
2024-07-26 22:59:36 -0700

    Comment model

---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const {model, Schema} = require('mongoose');
+mongoose.Schema.Types.String.set('trim', true);
+
+const userSchema = new Schema ( {
+    _id: mongoose.Schema.Types.ObjectId,
+    user: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+    ]
+    createdAt: {
+        type: Date,
+        trim: true,
+    }
+    rating: {
+        type: Number,
+        trim: true,
+    },
+    title: {
+        type: String,
+        trim: true,
+    }
+    description: {
+        type: String,
+        trim: true,
+    }
+
+}
+)
+module.exports = model('Comment', userSchema);
