6b29c21ae333b788e9fbc5deb41c9443e487a121
AlexFuaaark <alexfuaaark@icloud.com>
2024-07-29 20:29:36 -0700

    added generateId and fixed Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,32 +1,25 @@
-const mongoose = require('mongoose');
-const {model, Schema} = require('mongoose');
+const { model, Schema} = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Schema.Types.String.set('trim', true);
 
-const userSchema = new Schema ( {
-    _id: mongoose.Schema.Types.ObjectId,
-    user: [
-        {
+const commentSchema =
+
+    new Schema({
+        _id: mongoose.Schema.Types.ObjectId,
+
+        user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
         },
-    ],
-    createdAt: {
-        type: Date,
-        trim: true,
-    },
-    rating: {
-        type: Number,
-        trim: true,
-    },
-    title: {
-        type: String,
-        trim: true,
-    },
-    description: {
-        type: String,
-        trim: true,
-    }
 
-}
-)
-module.exports = model('Comment', userSchema);
+        createdAt: String,
+
+        rating: Number,
+
+        title: String,
+
+        description: String,
+
+    });
+
+module.exports = model('Comment', commentSchema)
