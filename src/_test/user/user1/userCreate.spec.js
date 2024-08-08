772d40af5a55e2579d9582d8b3d520343c92dfe4
AlexFuaaark <alexfuaaark@icloud.com>
2024-08-07 20:25:56 -0700

    fixed userCreate.spec and gqlRequest.js

--- src/_test/user/user1/userCreate.spec.js
+++ src/_test/user/user1/userCreate.spec.js
@@ -1,23 +1,23 @@
-
 const { expect } = require ('chai')
 const { userCreateQ } = require('./queries')
 const { user } = require('./data')
-const gqlRequest = require('../../gqlRequest')
+const gqlRequest = require('./gqlRequest')
 
-let respData: null = null;
+let respData = null;
 let postData = null;
+
 describe('USER CREATE', () => {
     describe('USER CREATE - POSITIVE TESTS', () => {
-        it( 'user create all fields', (done) => {
+        it( 'user create all fields',
+            (done) => {
             postData = {
                 query: userCreateQ,
                 variables: user
             }
-
             gqlRequest(postData)
                 .expect(200)
                 .end((err, res) => {
-                    if (err) return done (err)
+                    if (err) return done(err)
                     respData = res.body
                     console.log(respData);
                     //expect(respData).eq()
